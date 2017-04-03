f24d1785f7c38fb509dcc78f38c64d6635c0534b
TracyH19 <tracy.hernandez1902@gmail.com>
2017-04-03 15:07:48 -0600

    Update numberManager.h

--- numberManager.h
+++ numberManager.h
@@ -1,33 +1,31 @@
 #ifndef numberManager_h
 #define numberManager_h
 
-#include<math.h>
-#include<vector>
-#include<stdlib.h>
-#include<time.h>
+#include <math.h>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
 
-namespace numberManager
+using namespace std;
+
+class numberManager
 {
-	class numberManager
-	{
-		public:
-			numberManager();
-			virtual ~numberManager();
-			
-			vector<int>* product(vector<int>* vec, int multi, int columns, int init);
-			int sumVector(vector<int>* vec, int columns, int init);
-			int columnPrimeNumber(vector<int>* vec,  int columns, int init);
-			
-			int especificColumnPrimeNumber(vector<int>* vec, int column);
-		
-		protected:
-		
-		private:
-			bool isPrime(int n);
-		
-	}
-}
+    public:
+        numberManager();
+        virtual ~numberManager();
 
+        vector<int>* product(vector<int>* vec&, int multi);
+        int sumVector(vector<int>* vec, int columns, int init);
+        int columnPrimeNumber(vector<int>* vec,  int columns, int init);
 
+        int especificColumnPrimeNumber(vector<int>* vec, int column);
+		bool isPrime(int n);	
+    protected:
+
+    private:
+        
+
+};
 
 #endif // numberManager_h
